1d69210c3f57bb49a7df744c4edf8f549e080de7
William Luck <luck.william.h@gmail.com>
2022-03-16 12:42:03 -0500

    commit tree 66b26654a2c2d939ab3ac1b67ba3cb088461f672

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "Diego";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Tot";
+}
+
